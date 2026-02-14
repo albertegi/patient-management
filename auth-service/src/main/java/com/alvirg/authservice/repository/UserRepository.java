045b707b34ff761536dc3f09eb196dd46e5ad0cc
albertegi <albertegi90@gmail.com>
2026-02-14 23:23:27 +0000

    ** Create UserRepository to interact with the database to fetch, retrieve, update and save data

---
+++ auth-service/src/main/java/com/alvirg/authservice/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.alvirg.authservice.repository;
+
+import com.alvirg.authservice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+}
