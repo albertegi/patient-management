add5bbaf045bf776b6d49a23f3adc75838e528bc
albertegi <albertegi90@gmail.com>
2026-02-14 23:28:32 +0000

    inject UserService in the AuthService as part of dependency injection to be able to find a user by Email in the UserService.

--- auth-service/src/main/java/com/alvirg/authservice/service/AuthService.java
+++ auth-service/src/main/java/com/alvirg/authservice/service/AuthService.java
@@ -2,12 +2,16 @@ package com.alvirg.authservice.service;
 
 import com.alvirg.authservice.dto.LoginRequest;
 import com.alvirg.authservice.model.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class AuthService {
+
+    private final UserService userService;
 
     public Optional<String> authenticate(LoginRequest loginRequest){
         // first thing: get the user from the database
