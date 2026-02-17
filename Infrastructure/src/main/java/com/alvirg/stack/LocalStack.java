34e452d4336695ff042fbb0634590129dbdbc7f7
albertegi <albertegi90@gmail.com>
2026-02-17 16:48:43 +0000

    Add Health check for the databases in the infrastructure

--- Infrastructure/src/main/java/com/alvirg/stack/LocalStack.java
+++ Infrastructure/src/main/java/com/alvirg/stack/LocalStack.java
@@ -1,7 +1,12 @@
 package com.alvirg.stack;
 
 import software.amazon.awscdk.*;
+import software.amazon.awscdk.services.ec2.InstanceClass;
+import software.amazon.awscdk.services.ec2.InstanceSize;
+import software.amazon.awscdk.services.ec2.InstanceType;
 import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.rds.*;
+import software.amazon.awscdk.services.route53.CfnHealthCheck;
 
 public class LocalStack extends Stack {
     private final Vpc vpc;
@@ -10,12 +15,47 @@ public class LocalStack extends Stack {
         super(scope, id, props);
 
         this.vpc = createVpc();
+
+        DatabaseInstance authServiceDb = createDatabase("AuthServiceDB", "auth-service-db");
+        DatabaseInstance patientServiceDb = createDatabase("PatientServiceDB", "patient-service-db");
+
+        CfnHealthCheck authDbHealthCheck = createDbHealthCheck(authServiceDb, "AuthServiceDBHealthCheck");
+        CfnHealthCheck patientDbHealthCheck = createDbHealthCheck(patientServiceDb, "PatientServiceDBHealthCheck");
     }
 
     private Vpc createVpc(){
         return Vpc.Builder.create(this, "PatientManagementVPC")
                 .vpcName("PatientManagementVPC")
                 .maxAzs(2)
+                .build();
+    }
+
+    // This instance will be used to create the databases
+    private DatabaseInstance createDatabase(String id, String dbName){
+        return DatabaseInstance.Builder
+                .create(this, id)
+                .engine(DatabaseInstanceEngine.postgres(PostgresInstanceEngineProps.builder()
+                        .version(PostgresEngineVersion.VER_17_2)
+                        .build()))
+                .vpc(vpc)
+                .instanceType(InstanceType.of(InstanceClass.BURSTABLE2, InstanceSize.MICRO)) //instanceType is a combination of cpu, compute power that we want our databases to run on
+                .allocatedStorage(20) // the amount of storage we want the databases to run on
+                .credentials(Credentials.fromGeneratedSecret("admin_user"))// credentials will be used to connect to the database. This will take the username that we specify and generate a secret from this username and auto apply to the database
+                .databaseName(dbName)
+                .removalPolicy(RemovalPolicy.DESTROY)
+                .build();
+    }
+
+    // health check for databases
+    private CfnHealthCheck createDbHealthCheck(DatabaseInstance db, String id){
+        return CfnHealthCheck.Builder.create(this, id)
+                .healthCheckConfig(CfnHealthCheck.HealthCheckConfigProperty.builder()
+                        .type("TCP")
+                        .port(Token.asNumber(db.getDbInstanceEndpointPort())) // this will get the port that our database is running on. It will convert the string to a number
+                        .ipAddress(db.getDbInstanceEndpointAddress())
+                        .requestInterval(30) // check the health status every 30s
+                        .failureThreshold(3) // try 3 times once every 30s for every port failure
+                        .build())
                 .build();
     }
 
