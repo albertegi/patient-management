55652eb3c7a587b4705a108c6f1f0dde2e8c43ce
albertegi <albertegi90@gmail.com>
2026-02-18 03:00:24 +0000

    created all the services in the infrastructure using Fargate Service

--- Infrastructure/src/main/java/com/alvirg/stack/LocalStack.java
+++ Infrastructure/src/main/java/com/alvirg/stack/LocalStack.java
@@ -1,14 +1,20 @@
 package com.alvirg.stack;
 
+import com.amazonaws.services.licensemanager.model.LicenseStatus;
 import software.amazon.awscdk.*;
 import software.amazon.awscdk.services.ec2.*;
 import software.amazon.awscdk.services.ec2.InstanceType;
-import software.amazon.awscdk.services.ecs.CloudMapNamespaceOptions;
-import software.amazon.awscdk.services.ecs.Cluster;
+import software.amazon.awscdk.services.ecs.*;
+import software.amazon.awscdk.services.ecs.Protocol;
+import software.amazon.awscdk.services.logs.LogGroup;
+import software.amazon.awscdk.services.logs.RetentionDays;
 import software.amazon.awscdk.services.msk.CfnCluster;
 import software.amazon.awscdk.services.rds.*;
 import software.amazon.awscdk.services.route53.CfnHealthCheck;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class LocalStack extends Stack {
@@ -29,6 +35,45 @@ public class LocalStack extends Stack {
         CfnCluster mskCluster = createMskCluster();
 
         this.ecsCluster = createEcsCluster();
+
+        FargateService authService =
+                createFargateService("AuthService",
+                        "auth-service",
+                        List.of(8088),
+                        authServiceDb,
+                        Map.of("JWT_SECRETE", "sRilhKJvu9aLQbAq3v3VE9X3GyMEKMHOmhhTRygSo14E"));
+        authService.getNode().addDependency(authDbHealthCheck);
+        authService.getNode().addDependency(authServiceDb);
+
+        FargateService billingService =
+                createFargateService("BillingService",
+                        "billing-service",
+                        List.of(8086, 9096),
+                        null,
+                        null);
+
+        FargateService analyticsService =
+                createFargateService("AnalyticsService",
+                        "analytics-service",
+                        List.of(8087),
+                        null,
+                        null);
+        analyticsService.getNode().addDependency(mskCluster);
+
+        FargateService patientService =
+                createFargateService("PatientService",
+                        "patient-service",
+                        List.of(8085),
+                        patientServiceDb,
+                        Map.of(
+                                "BILLING_SERVICE_ADDRESS", "host.docker.internal",
+                                "BILLING_SERVICE_GRPC_PORT", "9096"
+                        ));
+
+        patientService.getNode().addDependency(patientServiceDb);
+        patientService.getNode().addDependency(patientDbHealthCheck);
+        patientService.getNode().addDependency(billingService);
+        patientService.getNode().addDependency(mskCluster);
     }
 
     private Vpc createVpc(){
@@ -88,6 +133,76 @@ public class LocalStack extends Stack {
                 .build();
     }
 
+    private FargateService createFargateService(String id,
+                                                String imageName,
+                                                List<Integer> ports,
+                                                DatabaseInstance db,
+                                                Map<String, String> additionalEnVars) {
+        // FargateService is a type of ECS service. there are a number of ECS services. It makes it easy to start, stop and scale ECS tasks
+        // The ECS task takes image name, and it will build a container from that image name and start the container
+
+        // An ECS task runs a container
+        // To create an ECS task, we need to create an ECSTaskDefinition.
+        // It's a blueprint that specifies the amount of CPU, memory, the image we want to use, environment variables
+
+        FargateTaskDefinition taskDefinition = FargateTaskDefinition.Builder.create(this, id + "Task")
+                .cpu(256)
+                .memoryLimitMiB(512)
+                .build();
+
+        ContainerDefinitionOptions.Builder containerOptions = ContainerDefinitionOptions.builder()
+                .image(ContainerImage.fromRegistry(imageName))
+                .portMappings(ports.stream().map(port -> PortMapping.builder()
+                                .containerPort(port)
+                                .hostPort(port)
+                                .protocol(Protocol.TCP)
+                                .build())
+                        .collect(Collectors.toList()))
+                .logging(LogDriver.awsLogs(AwsLogDriverProps.builder()
+                                .logGroup(LogGroup.Builder.create(this, id + "LogGroup")
+                                        .logGroupName("/ecs/" + imageName)
+                                        .removalPolicy(RemovalPolicy.DESTROY)
+                                        .retention(RetentionDays.ONE_DAY)
+                                        .build())
+                                .streamPrefix(imageName)
+                        .build()));
+
+
+        // define environment variables
+        Map<String, String> envVars = new HashMap<>();
+        envVars.put("SPRING_KAFKA_BOOTSTRAP_SERVERS", "localhost.localstack.cloud:4510, localhost.localstack.cloud:4511, localhost.localstack.cloud:4512");
+        if (additionalEnVars != null) {
+            envVars.putAll(additionalEnVars);
+        }
+
+        if(db != null){
+            envVars.put("SPRING_DATASOURCE_URL", "jdbc:postgresql://%s/%s/%s-db".formatted(
+                    db.getDbInstanceEndpointAddress(),
+                    db.getDbInstanceEndpointPort(),
+                    imageName
+            ));
+
+            envVars.put("SPRING_DATASOURCE_USERNAME", "admin_user");
+            envVars.put("SPRING_DATASOURCE_PASSWORD", db.getSecret().secretValueFromJson("password").toString());
+            envVars.put("SPRING_JPA_HIBERNATE_DLL_AUTO", "update");
+            envVars.put("SPRING_SQL_INIT_MODE", "always");
+            envVars.put("SPRING_DATASOURCE_HIKARI_INITIALIZATION_FAIL_TIMEOUT", "600000");
+        }
+
+        containerOptions.environment(envVars);
+        taskDefinition.addContainer(imageName + "Container", containerOptions.build());
+
+        // Now create a Fargate service that will run the ecs task
+
+        return FargateService.Builder.create(this, id)
+                .cluster(ecsCluster)
+                .taskDefinition(taskDefinition)
+                .assignPublicIp(false)
+                .serviceName(imageName)
+                .build();
+
+    }
+
     public static void main(final String[] args){
         App app = new App(AppProps.builder()
                 .outdir("./cdk.out")
@@ -105,4 +220,3 @@ public class LocalStack extends Stack {
 
 }
 
-// create a run configuration for the stack
